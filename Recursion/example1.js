ac49670029c0fcc903522021a0c63dd6279a8847
Ashad2020 <mdashadnstu97@gmail.som>
2021-09-22 14:51:51 +0600

    Base Condition Finding

--- Recursion/example1.js
+++ Recursion/example1.js
@@ -3,13 +3,18 @@ function isVowel(char) {
 }
 
 function countVowel(str) {
-  var count = 0;
-  for (var i = 0; i < str.length; i++) {
-    if (isVowel(str[i])) {
-      count++;
-    }
-  }
-  return count;
+  if (str.length == 0) return 0;
+  var first = (isVowel(str[0]) ? 1 : 0);
+  return first + countVowel( str.slice(1) );
 }
+// function countVowel(str) {
+//   var count = 0;
+//   for (var i = 0; i < str.length; i++) {
+//     if (isVowel(str[i])) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
 console.log(countVowel("The quick brown fox jumps over the lazy dog"));
